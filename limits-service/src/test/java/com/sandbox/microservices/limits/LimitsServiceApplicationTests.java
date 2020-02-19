131be7df82f6b7ab752e149fec2ff6aad2d91fe2
Andrii Sysoiev <andrii.sysoiev@gmail.com>
2020-02-19 20:22:12 +0200

    Init LimitsService

---
+++ limits-service/src/test/java/com/sandbox/microservices/limits/LimitsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandbox.microservices.limits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
