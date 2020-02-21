0f8cd7df4793bd905aec5f3222264d94a2ee253c
Andrii Sysoiev <andrii.sysoiev@gmail.com>
2020-02-21 20:29:16 +0200

    Refreshable configs

--- limits-service/src/main/java/com/sandbox/microservices/limits/controllers/LimitsConfigController.java
+++ limits-service/src/main/java/com/sandbox/microservices/limits/controllers/LimitsConfigController.java
@@ -3,12 +3,14 @@ package com.sandbox.microservices.limits.controllers;
 import com.sandbox.microservices.limits.configs.PropertiesConfiguration;
 import com.sandbox.microservices.limits.models.LimitsConfig;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author Andrii Sysoiev
  */
+@RefreshScope
 @RestController
 public class LimitsConfigController {
 
