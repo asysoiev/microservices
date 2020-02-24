e5ea7b84f4ebbda269db4e9a37eb6172d875cdab
Andrii Sysoiev <andrii.sysoiev@gmail.com>
2020-02-24 19:38:25 +0200

    Integrated with Ribbon

--- currency-conversion-service/src/main/java/com/sandbox/microservices/currency/conversion/feign/CurrencyExchangeClient.java
+++ currency-conversion-service/src/main/java/com/sandbox/microservices/currency/conversion/feign/CurrencyExchangeClient.java
@@ -1,6 +1,7 @@
 package com.sandbox.microservices.currency.conversion.feign;
 
 import com.sandbox.microservices.currency.conversion.models.ConversionResult;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,7 +9,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 /**
  * @author Andrii Sysoiev
  */
-@FeignClient(name = "currency-exchange-service", url = "http://localhost:8000/")
+@FeignClient(name = "currency-exchange-service")
+@RibbonClient(name = "currency-exchange-service")
 public interface CurrencyExchangeClient {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
