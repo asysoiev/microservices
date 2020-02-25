183ceade2c7a359e0a68ddd1105c7e4517ffedab
Andrii Sysoiev <andrii.sysoiev@gmail.com>
2020-02-25 20:27:19 +0200

    Init Eureka naming server

--- currency-conversion-service/src/main/java/com/sandbox/microservices/currency/conversion/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/sandbox/microservices/currency/conversion/CurrencyConversionServiceApplication.java
@@ -2,10 +2,12 @@ package com.sandbox.microservices.currency.conversion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("com.sandbox.microservices.currency.conversion.feign")
+@EnableDiscoveryClient
 public class CurrencyConversionServiceApplication {
 
     public static void main(String[] args) {
